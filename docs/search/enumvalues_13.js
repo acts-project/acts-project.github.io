b1dd16faf507769a8dfe2af9be82bc0af3b33d29
paulgessinger <paulgessinger@users.noreply.github.com>
2026-02-23 23:04:58 +0000

    Deploying to gh-pages from @ acts-project/acts-project.github.io@62cd943a35720f8e19639bb23f1a6fddb2eff408 🚀

--- docs/search/enumvalues_13.js
+++ docs/search/enumvalues_13.js
@@ -3,7 +3,6 @@ var searchData=
   ['variable_0',['Variable',['../namespace_acts.html#a2472b7e663ee623501edad0bbcd8a32fa47c14840d8e15331fa420b9b2f757cd9',1,'Acts']]],
   ['variancer_1',['VarianceR',['../namespace_acts.html#a7a6813a649567fb613b8214ad5e7399ca72bb52f0a15dd8981987d49e5e306c31',1,'Acts']]],
   ['variancez_2',['VarianceZ',['../namespace_acts.html#a7a6813a649567fb613b8214ad5e7399ca40d203bf0d98e07fc2dfbf12d880b13b',1,'Acts']]],
-  ['variancezr_3',['VarianceZR',['../namespace_acts.html#a7a6813a649567fb613b8214ad5e7399ca3aaf7978eaca2bad55d906b6f6d57f37',1,'Acts']]],
-  ['verbose_4',['VERBOSE',['../group__logging.html#gga9b12dd924a4bd3aed921a33779ece95ba7f2c44c8d8247eeb651a2b8137c51634',1,'Acts::Logging']]],
-  ['vertexz_5',['VertexZ',['../namespace_acts.html#a1f6eba2b6bc61b840fff13f9b3343edfa86eaa11de9571f1f395e715f9bf2c3f1',1,'Acts']]]
+  ['verbose_3',['VERBOSE',['../group__logging.html#gga9b12dd924a4bd3aed921a33779ece95ba7f2c44c8d8247eeb651a2b8137c51634',1,'Acts::Logging']]],
+  ['vertexz_4',['VertexZ',['../namespace_acts.html#a1f6eba2b6bc61b840fff13f9b3343edfa86eaa11de9571f1f395e715f9bf2c3f1',1,'Acts']]]
 ];
