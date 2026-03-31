eaf61e393d706180165e7d3ae1be96213fe9616f
paulgessinger <paulgessinger@users.noreply.github.com>
2026-03-31 07:41:58 +0000

    Deploying to gh-pages from @ acts-project/acts-project.github.io@62cd943a35720f8e19639bb23f1a6fddb2eff408 🚀

--- docs/search/pages_8.js
+++ docs/search/pages_8.js
@@ -6,7 +6,8 @@ var searchData=
   ['error_20enum_20pattern_3',['Error Enum Pattern',['../group__errors.html#autotoc_md3',1,'']]],
   ['error_20handling_20in_20acts_4',['Error Handling in ACTS',['../group__errors.html#autotoc_md1',1,'']]],
   ['event_20data_20model_5',['High-level Track Event Data Model',['../group__eventdata__tracks.html#edm_Tracks',1,'']]],
-  ['example_6',['Example',['../class_acts_fatras_1_1_barcode.html#autotoc_md76',1,'']]],
-  ['example_20scripts_7',['example scripts',['../md_docs_2pages_2examples_2python__bindings.html#autotoc_md62',1,'Python based example scripts'],['../python_bindings.html#autotoc_md68',1,'Python based example scripts']]],
-  ['examples_8',['Examples',['../python_bindings.html',1,'Python Bindings for Examples'],['../md_docs_2pages_2examples_2python__bindings.html',1,'Python bindings for the Examples']]]
+  ['example_6',['Example',['../class_acts_fatras_1_1_barcode.html#autotoc_md85',1,'']]],
+  ['example_7',['Comparison example',['../material_mapping_workflow.html#autotoc_md68',1,'']]],
+  ['example_20scripts_8',['example scripts',['../md_docs_2pages_2examples_2python__bindings.html#autotoc_md71',1,'Python based example scripts'],['../python_bindings.html#autotoc_md77',1,'Python based example scripts']]],
+  ['examples_9',['Examples',['../python_bindings.html',1,'Python Bindings for Examples'],['../md_docs_2pages_2examples_2python__bindings.html',1,'Python bindings for the Examples']]]
 ];
--- docs/search/pages_d.js
+++ docs/search/pages_d.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['j_20b_20r_20z_20m_0',['j B r z M',['../group__magnetic__field.html#autotoc_md72',1,'|| r | i || z | j || |B(r,z)| || M ||'],['../group__root__plugin.html#autotoc_md78',1,'|| r | i || z | j || |B(r,z)| || M ||']]],
-  ['j_20z_20k_20b_20x_20y_20z_20m_1',['j z k B x y z M',['../group__magnetic__field.html#autotoc_md73',1,'|| x | i || y | j || z | k || |B(x,y,z)| || M ||'],['../group__root__plugin.html#autotoc_md79',1,'|| x | i || y | j || z | k || |B(x,y,z)| || M ||']]]
+  ['j_20b_20r_20z_20m_0',['j B r z M',['../group__magnetic__field.html#autotoc_md81',1,'|| r | i || z | j || |B(r,z)| || M ||'],['../group__root__plugin.html#autotoc_md87',1,'|| r | i || z | j || |B(r,z)| || M ||']]],
+  ['j_20z_20k_20b_20x_20y_20z_20m_1',['j z k B x y z M',['../group__magnetic__field.html#autotoc_md82',1,'|| x | i || y | j || z | k || |B(x,y,z)| || M ||'],['../group__root__plugin.html#autotoc_md88',1,'|| x | i || y | j || z | k || |B(x,y,z)| || M ||']]]
 ];
